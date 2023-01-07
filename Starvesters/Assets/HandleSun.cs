9ceae0f5d3d570d4bb3a1622493fd92c117c574c
seb776 <sebastien.maire@epitech.eu>
2023-01-07 20:46:09 +0100

    Still fixing atmosphere parameters

--- Starvesters/Assets/HandleSun.cs
+++ Starvesters/Assets/HandleSun.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class HandleSun : MonoBehaviour
 {
     public Color SunColor;
+    public Light LightSource;
     void Update()
     {
         this.gameObject.GetComponent<MeshRenderer>().material.SetColor("_SunCol", SunColor);
+        LightSource.color = SunColor;
     }
 }
