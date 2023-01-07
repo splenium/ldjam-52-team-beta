769ab32c0eafa765e4e0bdc155c4b17fb28ae52f
seb776 <sebastien.maire@epitech.eu>
2023-01-07 20:15:47 +0100

    Added config for multiple planets to the HandlePlanetAtmosParams

--- Starvesters/Assets/HandlePlanetAtmosParams.cs
+++ Starvesters/Assets/HandlePlanetAtmosParams.cs
@@ -6,9 +6,38 @@ public class HandlePlanetAtmosParams : MonoBehaviour
 {
     public Material AtmosphereMaterial;
 
+    public GameObject PlanetA;
+    public float PlanetASize;
+    public Color PlanetAColorA;
+    public Color PlanetAColorB;
+
+    public GameObject PlanetB;
+    public float PlanetBSize;
+    public Color PlanetBColorA;
+    public Color PlanetBColorB;
+
+    public GameObject PlanetC;
+    public float PlanetCSize;
+    public Color PlanetCColorA;
+    public Color PlanetCColorB;
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        AtmosphereMaterial.SetVector("_PlanetAPos", PlanetA.transform.position);
+        AtmosphereMaterial.SetFloat("PlanetASize", PlanetASize);
+        AtmosphereMaterial.SetColor("_PlanetAColA", PlanetAColorA);
+        AtmosphereMaterial.SetColor("_PlanetAColB", PlanetAColorB);
+
+        AtmosphereMaterial.SetVector("_PlanetBPos", PlanetA.transform.position);
+        AtmosphereMaterial.SetFloat("PlanetBSize", PlanetASize);
+        AtmosphereMaterial.SetColor("_PlanetBColA", PlanetAColorA);
+        AtmosphereMaterial.SetColor("_PlanetBColB", PlanetAColorB);
+
+        AtmosphereMaterial.SetVector("_PlanetCPos", PlanetC.transform.position);
+        AtmosphereMaterial.SetFloat("PlanetCSize", PlanetCSize);
+        AtmosphereMaterial.SetColor("_PlanetCColA", PlanetCColorA);
+        AtmosphereMaterial.SetColor("_PlanetCColB", PlanetCColorB);
     }
 }
