38cda3a7266380dc15ab937ab78e8e6a97ff36fb
Herbigniaux <b.herbigniaux@hotmail.fr>
2023-01-07 14:28:18 +0100

    Add orbitLine and planet orbit

---
+++ Starvesters/Assets/Starvesters/Scripts/OrbitLine.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Starvesters.Scripts
+{
+    [ExecuteInEditMode]
+    public class OrbitLine : MonoBehaviour
+    {
+        [Range(3, 36)]
+        public int _segments;
+        public LineRenderer _lineRenderer;
+
+        private void CalculatePlanetEllipse()
+        {
+            Vector3[] points = new Vector3[_segments + 1];
+            var ellipse = GetOrbitEllipse();
+
+            for (int i = 0; i < _segments; i++)
+            {
+                Vector2 position2D = ellipse.Evaluate((float)i / (float)_segments);
+                points[i] = new Vector3(position2D.x, position2D.y, 0f);
+            }
+
+            points[_segments] = points[0];
+
+            _lineRenderer.positionCount = _segments + 1;
+            _lineRenderer.SetPositions(points);
+        }
+
+        public Ellipse GetOrbitEllipse()
+        {
+            // Le game object de l'elipse doit être au niveau du soleil
+            var distance = Vector3.Distance(transform.position, _lineRenderer.gameObject.transform.position);
+
+            return new Ellipse(distance);
+        }
+        public void Awake()
+        {
+            CalculatePlanetEllipse();
+        }
+
+        private void OnValidate()
+        {
+            CalculatePlanetEllipse();
+        }
+    }
+}
