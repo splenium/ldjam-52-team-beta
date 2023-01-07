874a36fe641d169b5056b1015acfdd76f7d39e0f
Herbigniaux <b.herbigniaux@hotmail.fr>
2023-01-07 15:57:19 +0100

    Update scene element and fix rotation of orbit

--- Starvesters/Assets/Starvesters/Scripts/OrbitLine.cs
+++ Starvesters/Assets/Starvesters/Scripts/OrbitLine.cs
@@ -18,7 +18,7 @@ namespace Assets.Starvesters.Scripts
             for (int i = 0; i < _segments; i++)
             {
                 Vector2 position2D = ellipse.Evaluate((float)i / (float)_segments);
-                points[i] = new Vector3(position2D.x, position2D.y, 0f);
+                points[i] = new Vector3(position2D.x, 0f, position2D.y);
             }
 
             points[_segments] = points[0];
