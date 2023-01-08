f04df9ab60dacb27287e2a2d3710cf41078570c8
seb776 <sebastien.maire@epitech.eu>
2023-01-08 18:55:02 +0100

    Removed useless code

--- Starvesters/Assets/Starvesters/Shaders/HandlePlanetAtmosphere.hlsl
+++ Starvesters/Assets/Starvesters/Shaders/HandlePlanetAtmosphere.hlsl
@@ -1,45 +1,5 @@
 #define sat(a) saturate(a)
 
-float2 raymarchSphere(float3 sphereCenter, float sphereRadius, float3 rayOrigin, float3 rayDir)
-{
-	float3 p = rayOrigin;
-	float dNear = 0.;
-	if (distance(sphereCenter, rayOrigin) > sphereRadius)
-	{
-
-		for (int i = 0; i < 128; ++i)
-		{
-			float d = length(p - sphereCenter) - sphereRadius;
-			if (d < 0.01)
-			{
-				dNear = distance(rayOrigin, p);
-				break;
-			}
-			p += rayDir * d;
-		}
-	}
-	float dFar = 0.;
-
-	//if (dNear != -1.)
-	{
-		//if (distance(p1, rayOrigin) > 0.01)
-		p += -normalize(p - sphereCenter) * 0.05; // avoid hitting the sphere
-		for (int i = 0; i < 32; ++i)
-		{
-			float d = -(length(p - sphereCenter) - sphereRadius); // we inverse the shape to calculate the other intersection
-			if (d < 0.01)
-			{
-				dFar = distance(rayOrigin, p);
-				break;
-			}
-			p += rayDir * d;
-		}
-	}
-	return float2(dNear, dFar);
-}
-
-
-
 float2 RaySphereIntersection(float3 sphereCenter, float sphereRadius, float3 rayOrigin, float3 rayDir)
 {
 	float3 offset = sphereCenter - rayOrigin;
@@ -64,32 +24,6 @@ float2 RaySphereIntersection(float3 sphereCenter, float sphereRadius, float3 ray
 }
 void HandlePlanetAtmosphere_hlsl_float(float2 uv, float3 inputCol, float depth, float planetSize, float3 planetPos, float3 viewDir, float3 worldSpaceCameraPos, float3 planetColA, float3 planetColB, out float3 col)
 {
-	if (false)//uv.x < 0.5)
-	{
-
-		// inputCol += float3(.2,0.,0.);
-		float2 hit = raymarchSphere(planetPos, planetSize, worldSpaceCameraPos, -viewDir);
-		float3 hitPos = worldSpaceCameraPos + hit.x * viewDir;
-		//inputCol += float3(1., 0., 0.);
-		float distToAtmos = hit.x;
-		float distThrough = min(hit.y - hit.x, depth - distToAtmos);
-		float coefScatter = (distThrough) / (planetSize * 2.);
-		float3 pcolshadow = planetPos + normalize(planetPos) * .1;//*planetSize; // origin of the center of the gradient
-		float3 rgb = viewDir;
-		coefScatter = pow(coefScatter, lerp(2., 3., saturate(depth / 1000.))) * 2.5 * (1. - sat(distToAtmos / 10.));
-		float oklerp = 1. - sat(dot(normalize(hitPos - planetPos), normalize(planetPos)));
-		planetColA *= sat((length(hitPos + normalize(planetPos) * .5) - planetSize * .5) * 10.);
-		float oklerp2 = sat(dot(viewDir, normalize(hitPos - planetPos)));
-		rgb = lerp(planetColA, planetColB, oklerp);
-		rgb *= oklerp2;
-		rgb *= 1. + pow(sat(dot(normalize(planetPos), viewDir) - .95), 2.5) * 1500. * float3(224, 182, 34) / 255.;
-		col = lerp(inputCol, rgb, sat(coefScatter));
-		col = hit.x*float3(1.,0.,0.);
-	}
-	else
-	{
-
-		// inputCol += float3(.2,0.,0.);
 		float2 hit = RaySphereIntersection(planetPos, planetSize, worldSpaceCameraPos, viewDir);
 		float3 hitPos = worldSpaceCameraPos + hit.x * viewDir;
 		float dstToAtmos = hit.x;
@@ -102,7 +36,5 @@ void HandlePlanetAtmosphere_hlsl_float(float2 uv, float3 inputCol, float depth, 
 		float oklerp2 = sat(dot(viewDir, normalize(hitPos - planetPos)));
 		float3 rgb = lerp(planetColA, planetColB, oklerp);
 		col = lerp(inputCol, rgb,factor);
-	}
-		
-//		col = inputCol;
+
 }
