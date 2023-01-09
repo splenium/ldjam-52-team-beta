6066774e8cd86d4146471915aa5fabf2328bb1db
seb776 <sebastien.maire@epitech.eu>
2023-01-09 17:08:26 +0100

    Completed planet atmospheres

--- Starvesters/Assets/Starvesters/Shaders/HandlePlanetAtmosphere.hlsl
+++ Starvesters/Assets/Starvesters/Shaders/HandlePlanetAtmosphere.hlsl
@@ -2,7 +2,7 @@
 
 float2 RaySphereIntersection(float3 sphereCenter, float sphereRadius, float3 rayOrigin, float3 rayDir)
 {
-	float3 offset = sphereCenter - rayOrigin;
+	float3 offset = rayOrigin-sphereCenter;
 	float a = 1.0;// dot(rayDir, rayDir); // set to dot (raydir, raydir) if raydir is not normalized
 	float b = 2.0 * dot(offset, rayDir);
 	float c = dot(offset, offset) - sphereRadius * sphereRadius;
@@ -22,19 +22,85 @@ float2 RaySphereIntersection(float3 sphereCenter, float sphereRadius, float3 ray
 	}
 	return float2(100000., 0.); // We did not hit
 }
+float _densityFalloff;
+float3 _planetPos;
+float _planetRadius;
+float _atmosRadius;
+// TODO densityFalloff setting
+float densityAtPoint(float3 densitySamplePoint)
+{
+
+	float heightAboveSurface = length(densitySamplePoint - _planetPos) - _planetRadius;
+	float height01 = heightAboveSurface / (_atmosRadius - _planetRadius);
+	float localDensity = exp(-height01 * _densityFalloff)*(1.-height01);
+	return localDensity;
+}
+
+float opticalDepth(float3 rayOrigin, float3 rayDir, float rayLength)
+{
+	int densityPointCnt = 10;
+	float3 densitySamplePoint = rayOrigin;
+	float stepSize = rayLength / (densityPointCnt - 1);
+	float opticalDepth = 0.;
+
+	for (int i = 0; i < densityPointCnt; ++i)
+	{
+		float localDensity = densityAtPoint(densitySamplePoint);
+		opticalDepth += localDensity * stepSize;
+		densitySamplePoint += rayDir * stepSize;
+	}
+	return opticalDepth;
+
+}
+
+float3 _scatterCoefs;
+float3 calculateLight(float3 rayOrigin, float3 rayDir, float rayLength)
+{
+	int scatteringPointCnt = 10;
+	float3 inScatterPoint = rayOrigin;
+	float stepSize = rayLength / (scatteringPointCnt - 1);
+	float3 inScatterLight = 0.;
+
+	for (int i = 0; i < scatteringPointCnt;++i)
+	{
+		float3 dirToSun = -normalize(inScatterPoint); // assume sun is 0 0 0 // unsure
+		float sunRayLength = RaySphereIntersection(_planetPos, _atmosRadius, inScatterPoint, dirToSun).y;
+		float sunRayOpticalDepth = opticalDepth(inScatterPoint, dirToSun, sunRayLength);
+		float viewRayOpticalDepth = opticalDepth(inScatterPoint, -rayDir, stepSize*i);
+		float3 transmittance = exp(-(sunRayOpticalDepth+viewRayOpticalDepth)*_scatterCoefs);
+		float localDensity = densityAtPoint(inScatterPoint);
+		inScatterLight += localDensity * transmittance * stepSize;
+		inScatterPoint += rayDir * stepSize;
+	}
+	return inScatterLight;//saturate(dot(normalize(rayOrigin), -normalize(_planetPos)));//inScatterLight;
+}
+
 void HandlePlanetAtmosphere_hlsl_float(float2 uv, float3 inputCol, float depth, float planetSize, float3 planetPos, float3 viewDir, float3 worldSpaceCameraPos, float3 planetColA, float3 planetColB, out float3 col)
 {
-		float2 hit = RaySphereIntersection(planetPos, planetSize, worldSpaceCameraPos, viewDir);
-		float3 hitPos = worldSpaceCameraPos + hit.x * viewDir;
-		float dstToAtmos = hit.x;
+	_scatterCoefs=planetColA;
+			_densityFalloff = 10.0f;
+			_planetPos = planetPos;
+			_planetRadius = planetSize;
+			_atmosRadius = _planetRadius+_planetRadius*2.1f;
+		viewDir = -viewDir;
+		float2 hit = RaySphereIntersection(planetPos, _atmosRadius, worldSpaceCameraPos, viewDir);
+				float dstToAtmos = hit.x;
 		float dstThroughAtmos = min(hit.y, depth - dstToAtmos);
-		float factor = (dstThroughAtmos / (planetSize * 2.0));
-		factor = pow(sat(factor), 2.0);
-		float oklerp = 1. - sat(dot(normalize(hitPos - planetPos), normalize(planetPos)));
 
-		planetColA *= sat((length(hitPos + normalize(planetPos) * .5) - planetSize * .5) * 10.);
-		float oklerp2 = sat(dot(viewDir, normalize(hitPos - planetPos)));
-		float3 rgb = lerp(planetColA, planetColB, oklerp);
-		col = lerp(inputCol, rgb,factor);
+		float3 rgb = 0.;//lerp(planetColA, planetColB, oklerp);
+
+		if (dstThroughAtmos > 0.)
+		{
+
+
+			float epsilon = 0.001;
+			float3 pointInAtmos = worldSpaceCameraPos + viewDir * (dstToAtmos+epsilon);
+			rgb = calculateLight(pointInAtmos, viewDir, dstThroughAtmos-epsilon*2)*planetColA;
+			col = inputCol * (1.-rgb)+rgb;
+
+			return;
+		}
+		
+		col = inputCol;//lerp(inputCol, rgb,factor);
 
 }
